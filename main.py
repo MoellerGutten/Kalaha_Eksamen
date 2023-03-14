b8c3a7337bd9718c4ed79c3036cb936f7a0dca68
MoellerGutten <93318104+MoellerGutten@users.noreply.github.com>
2023-03-14 12:27:00 +0100

    Button class added til programmet

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ pi = math.pi
 res = [width, height]
 
 white = (255, 255, 255)
+grey = (105,105,105)
+black = (0,0,0)
 screen_display = pg.display
 
 window_icon = pg.image.load( "window_icon.png")
@@ -23,6 +25,38 @@ surface = screen_display.set_mode(res)
 ball = pg.image.load("Kalaha_Kugle.png")
 
 gamestate = "start_menu"
+
+
+class button():
+    def __init__(self, color, x, y, button_width, button_height,  text=''):
+        self.color = color
+        self.x = x
+        self.y = y
+        self.button_width = button_width
+        self.button_height = button_height
+        self.text = text
+
+
+    def draw(self, surface, border_width, outline):
+        # Call this method to draw the button on the screen
+        if outline and border_width:
+            pg.draw.rect(surface, outline, (self.x - border_width, self.y - border_width, self.button_width + (border_width*2), self.button_height + (border_width*2)), 0)
+
+        pg.draw.rect(surface, self.color, (self.x, self.y, self.button_width, self.button_height), 0)
+
+        if self.text != '':
+            font = pg.font.SysFont('comicsans', 40)
+            text = font.render(self.text, True, (white))
+            surface.blit(text, (self.x + (self.button_width / 2 - text.get_width() / 2), self.y + (self.button_height / 2 - text.get_height() / 2)))
+
+
+    def isOver(self, pos):
+        # Pos is the mouse position or a tuple of (x,y) coordinates
+        if pos[0] > self.x and pos[0] < self.x + self.button_width:
+            if pos[1] > self.y and pos[1] < self.y + self.button_height:
+                return True
+
+        return False
 
 
 def generate(n):
@@ -64,17 +98,19 @@ def generate(n):
 
 def draw_game():
     surface.fill(white)
-    generate(10)
+    generate(7)
     screen_display.update()
 
 
 def draw_start_screen():
     surface.fill(white)
     font = pg.font.SysFont('arial', 40)
-    title = font.render('Kalaha', True, (0,0,0))
-    start_button = font.render('Start', True, (0,0,0))
+    title = font.render('Kalaha', True, black)
+    start_button = font.render('Start', True, black)
     surface.blit(title, (width / 2 - title.get_width() / 2, height / 2 - title.get_height() / 2))
     surface.blit(start_button, (width / 2 - start_button.get_width() / 2, height/ 2 + start_button.get_height() / 2))
+    button1 = button(black, 100,100,100,50,"hej")
+    button1.draw(surface, 5, outline=grey)
     pg.display.update()
 
 
