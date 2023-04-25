121dc34f6fe0245724e277fd812f8a29662eee09
G00dbo1 <aboennelykke@gmail.com>
2023-04-25 14:33:32 +0200

    Lavet botten bedre

--- bot_Consol_kalaha.py
+++ bot_Consol_kalaha.py
@@ -9,6 +9,12 @@ def print_board():
     print(f"{board[13]} |{board[12]}|{board[11]}|{board[10]}|{board[9]}|{board[8]}|{board[7]}| {board[6]}\n")
 
 
+def test_print(some_board):
+    print(f"Dette er et test af board {some_board}")
+    print(f"|{some_board[0]}|{some_board[1]}|{some_board[2]}|{some_board[3]}|{some_board[4]}|{some_board[5]}|  ")
+    print(f"{some_board[13]} |{some_board[12]}|{some_board[11]}|{some_board[10]}|{some_board[9]}|{some_board[8]}|{some_board[7]}| {some_board[6]}\n")
+
+
 # Checks if the game has ended and printes out who won
 def check_win():
     if board[12] + board[11] + board[10] + board[9] + board[8] + board[7] == 0 or board[0] + board[1] + board[2] + \
@@ -26,80 +32,80 @@ def check_win():
 
 
 def bot_move():
-    moves = {}
-    test_board = []
+    future_moves = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
+    enemy_future_moves = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
     for i in range(6):
-        if i == 0:
-            moves, empty = minimax(True)
-        else:
-            new_moves, new_board = minimax(True, test_board, moves)
-            moves = new_moves
-            test_board = new_board
-    if moves == "done":
+        test_board = []
+        for element in board:
+            test_board.append(element)
+        score, motified_board = try_move(test_board, i, test_board[i], False)
+        future_moves[i] += score
+        enemy_move = minimax(False, motified_board)
+        enemy_score, motified_board = try_move(motified_board, enemy_move, motified_board[enemy_move], True)
+        enemy_future_moves[i] += enemy_score
+        test_board = motified_board
+        for y in range(6):
+            bmove = minimax(True, test_board)
+            score, motified_board = try_move(test_board, bmove, test_board[bmove], False)
+            future_moves[i] += score
+            enemy_move = minimax(False, motified_board)
+            enemy_score, motified_board = try_move(motified_board, enemy_move, motified_board[enemy_move], True)
+            enemy_future_moves[i] += enemy_score
+            test_board = motified_board
+    print(future_moves)
+    bmove = max(future_moves, key=future_moves.get)
+    if bmove == "done":
         return "Stop"
-    if max(moves.values()) == -1 or max(moves.values()) == 0:
+    if bmove == -1 or bmove == 0:
         while True:
             ran_num = random.randint(0, 5)
             if board[ran_num] != 0:
                 return ran_num
     else:
-        print(moves)
-        return max(moves, key=moves.get)
+        return bmove
 
 
-def minimax(max_turn, tested_board=None, best_possible_move=None):
+def minimax(max_turn, tested_board):
     enemy_best_possible_move = {}
-    motified_board = []
-    if best_possible_move is None:
-        best_possible_move = {}
-        has_seleted_values = False
-    else:
-        has_seleted_values = True
+    best_possible_move = {}
     if check_win():
         return "Done"
     if max_turn:
-        if best_possible_move is not None:
-            i = 0
-            tested_board = []
-            for element in board:
-                tested_board.append(element)
-                i += 1
-            start_score = tested_board[13]
-        else:
-            start_score = tested_board[13]
+        start_score = tested_board[13]
         for y in range(6):
             outcome = 0
             score, motified_board = try_move(tested_board, y, tested_board[y], False)
-            if score > start_score:
-                outcome += 1
             if score > start_score + 1:
                 outcome += 2
+            elif score > start_score:
+                outcome += 1
             else:
                 outcome -= 1
-            min_list = minimax(False, motified_board)
-            if score > max(min_list.values()) + 2:
-                outcome += 1
-            if has_seleted_values:
-                best_possible_move[y] = outcome + best_possible_move[y]
-            else:
-                best_possible_move[y] = outcome
-        return best_possible_move, motified_board
+            # min_val = minimax(False, motified_board)
+            best_possible_move[y] = outcome
+        best_move = max(best_possible_move, key=best_possible_move.get)
+        return best_move
     else:
+        start_score = tested_board[6]
         for y in range(6):
             outcome = 0
-            start_score = tested_board[6]
-            score, motified_board = try_move(tested_board, y, tested_board[y], False)
-            if score > start_score:
-                outcome += 1
+            score, motified_board = try_move(tested_board, y, tested_board[y], True)
             if score > start_score + 1:
                 outcome += 2
+            elif score > start_score:
+                outcome += 1
             else:
                 outcome -= 1
+            # min_list = minimax(True, motified_board)
             enemy_best_possible_move[y] = outcome
-        return enemy_best_possible_move
+        enemy_best_move = max(enemy_best_possible_move, key=enemy_best_possible_move.get)
+        return enemy_best_move
 
 
-def try_move(new_board, location, amount, player1_turn):
+def try_move(old_board, location, amount, player1_turn):
+    new_board = []
+    for element in old_board:
+        new_board.append(element)
     # If the player choices a hole that has 0 ball, it chocies the hole opposite
     if amount == 0:
         # dist is asigned the amount of hole it is from player1's goal
@@ -133,7 +139,7 @@ def try_move(new_board, location, amount, player1_turn):
 
             # Checks if the last ball went into the players goal. if it did, it gives the player another turn
             if location == 6 and i + 1 == amount:
-                new_board[6] += 2
+                new_board[6] += 3
 
         send_board = []
         for element in new_board:
@@ -160,7 +166,7 @@ def try_move(new_board, location, amount, player1_turn):
 
             # Checks if the last ball went into the players goal. if it did, it gives the player another turn
             if (location == 13 or location == -1) and i + 1 == amount:
-                new_board[13] += 2
+                new_board[13] += 1
 
         send_board = []
         for element in new_board:
